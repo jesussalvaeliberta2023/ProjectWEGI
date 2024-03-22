793d91589257843826e0551817cd3b81d2ac6bd9
Wellington <wellington.gts21@gmail.com>
2024-03-22 15:42:49 -0300

    CODE

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Modal, Button, Image } from "react-native";
+import { useState } from "react";
+import { useEffect } from "react";
+import styles from "./src/styles/StyleSheet";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  visModal = (vis) => {
+    if (!visible) {
+      setVisible(vis);
+    } else {
+      setVisible(vis);
+    }
+  };
+
+  // O useEffect fica observando a variável...
+  useEffect(() => {
+    if (name.length === 29) {
+      alert("OH, NO! YOU CAN´T DO IIIIT!");
+    }
+  }, [name]);
+
+  changeTxt = (textInput) => {
+    setName(`Seja bem-vindo, ${textInput}!`);
+    console.log(name.length);
+  };
+
   return (
+    //Essa é a View principal, que ocupa toda a tela
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Esse é o botão que vai abrir o meu modal e vai deixar a visibilidade como "true" */}
+      <Image
+        style={styles.img}
+        source={require("./src/assets/images/logo.png")}
+      />
+
+      <Text style={styles.titulo}>Qual a melhor opção?</Text>
+      <Button title="Abrir modal" onPress={() => visModal(true)} />
+
+      {/* true or false*/}
+      <Modal transparent={true} animationType="fade" visible={visible}>
+        <View style={styles.container}>
+          <View style={styles.modal}>
+            <Text style={styles.texto}>Isabella ;D</Text>
+            <Button title="Fecha esse trem" onPress={() => visModal(false)} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
